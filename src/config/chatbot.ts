4a08cba5340a160572ed3c772277c75a320f7a1f
yvexanagency <yvexanagency@gmail.com>
2026-04-21 09:56:05 +0100

    Update chatbot.ts

--- src/config/chatbot.ts
+++ src/config/chatbot.ts
@@ -20,7 +20,7 @@ export const chatbotConfig: ChatbotConfig = {
       var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
       v.onload = function() {
         window.voiceflow.chat.load({
-          verify: { projectID: '69da8224553d9e92e85d401f' },
+          verify: { projectID: '69e73aaefc8dd0daf4d1b866' },
           url: 'https://general-runtime.voiceflow.com',
           versionID: 'production',
           voice: {
